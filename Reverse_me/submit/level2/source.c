2e384917b11f83283bc22620097be47700ff6761
keiseiisobe <keisei.isobe@gmail.com>
2025-05-18 22:13:51 +0900

    modified source.c

--- Reverse_me/submit/level2/source.c
+++ Reverse_me/submit/level2/source.c
@@ -3,23 +3,49 @@
 #include <string.h>
 
 void no() {
-  printf("Nope.");
+  printf("Nope.\n");
   exit(1);
+}
+
+void yes() {
+  printf("Good job.\n");
 }
 
 int main() {
   printf("Please enter key: ");
-  char v[20];
-  if (scanf("%19s", v) != 1)
+  char str2[30];
+  if (scanf("%29s", str2) != 1)
     no();
-  if (v[0] != 0x30)
+  if (str2[0] != 0x30)
     no();
-  if (v[1] != 0x30)
+  if (str2[1] != 0x30)
     no();
   fflush(NULL);
-  char buffer[9];
-  memset(buffer, 0, 9);
-  while (1) {
-    
+  char str[9];
+  memset(str, 0, 9);
+  str[0] = 'd';
+  size_t i = 1;
+  size_t i2 = 2;
+  size_t len = strlen(str2);
+  printf("len: %lu\n", len);
+  while (i2 + 2 < len) {
+    printf("i2: %lu\n", i2);
+    char str3[4];
+    size_t i3 = 0;
+    str3[i3] = str2[i2];
+    str3[i3 + 1] = str2[i2 + 1];
+    str3[i3 + 2] = str2[i2 + 2];
+    str3[i3 + 3] = '\0';
+    str[i] = atoi(str3);
+    i++;
+    i2 += 3;
+    if (strlen(str) >= 8)
+      break;
   }
+  printf("%s: %lu bytes\n", str, strlen(str));
+  if (strcmp(str, "delabere") == 0)
+    yes();
+  else
+    no();
+  return 0;
 }
